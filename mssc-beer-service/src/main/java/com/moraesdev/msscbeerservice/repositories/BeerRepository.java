2b7bcaa8576ecdf76fa97d27654bed11dbe9b9ef
jpmoraes97 <lookinhoo@hotmail.com>
2020-04-26 16:17:15 -0300

    adding spring data jpa repository for beer objects. closes #4

---
+++ mssc-beer-service/src/main/java/com/moraesdev/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.moraesdev.msscbeerservice.repositories;
+
+import com.moraesdev.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
