d31774e65648c88b1c5c6fb5779b88af98559241
jpmoraes97 <lookinhoo@hotmail.com>
2020-04-26 16:41:18 -0300

    adding boostrap data. closes #5

---
+++ mssc-beer-service/src/main/java/com/moraesdev/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,46 @@
+package com.moraesdev.msscbeerservice.bootstrap;
+
+import com.moraesdev.msscbeerservice.domain.Beer;
+import com.moraesdev.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(849859839284984L)
+                    .price(new BigDecimal("12.90"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(839859839284984L)
+                    .price(new BigDecimal("12.90"))
+                    .build());
+        }
+    }
+}
