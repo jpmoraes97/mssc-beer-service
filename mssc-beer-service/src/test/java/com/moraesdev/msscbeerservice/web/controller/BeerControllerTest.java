28cbed8ae924f0272885b0b8e235ea191c76c7cf
jpmoraes97 <lookinhoo@hotmail.com>
2020-04-26 16:07:18 -0300

    adding beer entity. closes #3

---
+++ mssc-beer-service/src/test/java/com/moraesdev/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,25 @@
+package com.moraesdev.msscbeerservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getBeerById() {
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeer() {
+    }
+}
