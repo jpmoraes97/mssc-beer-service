60518f3bdb5a78b91f329ca34bfe82d03fcfb306
jpmoraes97 <lookinhoo@hotmail.com>
2020-05-06 20:30:27 -0300

    Adding EhCache. closes #17

--- mssc-beer-service/src/main/java/com/moraesdev/msscbeerservice/services/BeerServiceImpl.java
+++ mssc-beer-service/src/main/java/com/moraesdev/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import com.moraesdev.msscbeerservice.web.model.BeerDto;
 import com.moraesdev.msscbeerservice.web.model.BeerPagedList;
 import com.moraesdev.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,10 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
 
@@ -66,8 +69,10 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
+
         if (showInventoryOnHand) {
             return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId)
                     .orElseThrow(NotFoundException::new));
